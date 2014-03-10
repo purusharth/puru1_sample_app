0fb29f1a57a1062a703e5f04d1f6d34606944888
Purusharth Prakash <puru1@umbc.edu>
2014-03-09 21:27:46 -0400

    Completed Ch 9 Ex 6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,17 +12,27 @@ class UsersController < ApplicationController
   end
   
   def new
-  	@user = User.new
+    # Ch9 Ex 6
+    if signed_in?
+      redirect_to root_url
+    else
+      @user = User.new
+    end
   end
 
   def create
-    @user = User.new(user_params)    
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+    # Ch9 Ex 6
+    if signed_in?
+      redirect_to root_url
     else
-      render 'new'
+      @user = User.new(user_params)    
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
     end
   end
 
